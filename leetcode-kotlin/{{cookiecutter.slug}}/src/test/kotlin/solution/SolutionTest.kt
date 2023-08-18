0ff66072267537a7f7b5ec97f32cdc09d50dafa4
Petr Korobeinikov <pkorobeinikov@gmail.com>
2023-08-18 09:11:19 +0400

    Use sut as test instance name for Kotlin

--- leetcode-kotlin/{{cookiecutter.slug}}/src/test/kotlin/solution/SolutionTest.kt
+++ leetcode-kotlin/{{cookiecutter.slug}}/src/test/kotlin/solution/SolutionTest.kt
@@ -1,12 +1,10 @@
 package solution
 
 import kotlin.test.Test
-import kotlin.test.assertTrue
 
 class SolutionTest {
   @Test
   fun solution() {
-    val classUnderTest = Solution()
-    assertTrue(classUnderTest.solution(), "someLibraryMethod should return 'true'")
+    val sut = Solution()
   }
 }
