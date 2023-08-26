19c811a9898e7c53af922ff64c50fdac7f3ea93c
Petr Korobeinikov <pkorobeinikov@gmail.com>
2023-08-26 21:14:30 +0400

    Get rid of long test name for swift

--- leetcode-swift/{{cookiecutter.slug}}/Tests/SolutionTests/SolutionTests.swift
+++ leetcode-swift/{{cookiecutter.slug}}/Tests/SolutionTests/SolutionTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import Solution
 
 final class SolutionTests: XCTestCase {
-    func testExample() throws {
+    func test() throws {
         XCTAssertEqual(Solution().solution(), false)
     }
 }
