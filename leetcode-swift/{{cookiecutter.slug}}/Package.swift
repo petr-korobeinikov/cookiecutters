ce3a1a202f9f3671450216bc30a6502522704a7d
Petr Korobeinikov <pkorobeinikov@gmail.com>
2024-03-12 08:12:26 +0300

    Add Swift Collections dependency
    Signed-off-by: Petr Korobeinikov <pkorobeinikov@gmail.com>

--- leetcode-swift/{{cookiecutter.slug}}/Package.swift
+++ leetcode-swift/{{cookiecutter.slug}}/Package.swift
@@ -14,13 +14,17 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/devxoul/Then", exact: "3.0.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Solution",
-            dependencies: ["Then"]
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections"),
+                "Then",
+            ]
         ),
         .testTarget(
             name: "SolutionTests",
